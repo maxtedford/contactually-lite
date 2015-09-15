9091ecc6194c77e4f8afcd1d58443fc6f6f4c52d
Max Tedford <max.tedford@gmail.com>
2015-09-15 15:05:57 -0600

    add file submission field to welcome page with corresponding specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root "welcome#show"
+  
+  resources :contacts, only: [:create]
 end
--- spec/features/user_welcome_spec.rb
+++ spec/features/user_welcome_spec.rb
@@ -3,10 +3,22 @@ require "rails_helper"
 describe "the user", type: :feature do
   context "when visiting the root page" do
     
+    before(:each) { visit root_path }
+    
     it "will see a welcome message" do
-      visit root_path
-      
       expect(page).to have_content("Welcome to Contactually-Lite!")
+    end
+    
+    it "will see a file upload invitation" do
+      expect(page).to have_content("Upload your contacts here:")
+    end
+    
+    it "will see an input field for contacts" do
+      expect(page).to have_css('input[type="file"]')
+    end
+    
+    it "will see a submit button" do
+      expect(page).to have_button("upload")
     end
   end
 end
