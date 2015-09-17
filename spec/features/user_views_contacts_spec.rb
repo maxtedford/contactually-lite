83fed4948031301e9932d2bd7f0e8829fafffff1
Max Tedford <max.tedford@gmail.com>
2015-09-17 15:56:22 -0600

    add specs to cover buttons appearing on page

--- spec/features/user_views_contacts_spec.rb
+++ spec/features/user_views_contacts_spec.rb
@@ -25,5 +25,30 @@ describe "the user", type: :feature do
       expect(page).to have_button("Delete")
       expect(page.all(:css, 'button.delete-button').length).to eq(200)
     end
+
+    it "can see a filter button for international phone numbers" do
+      expect(page).to have_button("International Numbers")
+      expect(page.all(:css, 'button#international-filter-button').length).to eq(1) 
+    end
+    
+    it "can see a filter button for phone numbers with extensions" do
+      expect(page).to have_button("Contacts with Extensions")
+      expect(page.all(:css, 'button#extension-filter-button').length).to eq(1)
+    end
+
+    it "can see a filter button for emails ending in .com" do
+      expect(page).to have_button("Contacts with .com Email Address")
+      expect(page.all(:css, 'button#dot-com-filter-button').length).to eq(1)
+    end
+
+    it "can see a button to sort contacts by email" do
+      expect(page).to have_button("Sort by Email Address")
+      expect(page.all(:css, 'button#sort-by-email-address').length).to eq(1)
+    end
+
+    it "can see a filter button for all contacts" do
+      expect(page).to have_button("All")
+      expect(page.all(:css, 'button#all').length).to eq(1)
+    end
   end
 end
