93de0ecf4763244707cac340317dda9b6ac7d98c
Max Tedford <max.tedford@gmail.com>
2015-09-16 21:15:48 -0600

    add international phone number filter

--- app/assets/javascripts/contacts_list.js
+++ app/assets/javascripts/contacts_list.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   bindDeleteHandlers();
+  bindInternationalFilterHandler();
 });
 
 function bindDeleteHandlers() {
@@ -20,4 +21,21 @@ function deleteContact(contact) {
       contact.remove();
     }
   })
+}
+
+function bindInternationalFilterHandler() {
+  $('#international-filter-button').on('click', function(event) {
+    event.preventDefault();
+    filterInternationalContacts();
+  })
+}
+
+function filterInternationalContacts() {
+  var $contacts = $('.contact');
+  var $domesticContacts = $contacts.filter(function(_index, _contact) {
+    return $(this).data().international;
+  });
+  $domesticContacts.each(function(_index, _contact) {
+    $(this).toggle();
+  });
 }
